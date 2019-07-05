34dd9d69318727c23e2d305b7ad82cea6270d299
hanihanihaani <1207944088@qq.com>
2019-07-05 18:57:29 +0800

    优化

--- src/tmp/nine-tml/index.js
+++ src/tmp/nine-tml/index.js
@@ -1,15 +1,20 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Image } from '@tarojs/components'
+import AllSwiper from '@components/swiper'
 import utils from '@utils/util'
 import './index.scss'
 
 export default class NineTml extends Component {
+  static defaultProps = {
+    list:[]
+  }
   goColumn(e) {
     utils.jump(e.currentTarget.id)
   }
   render () {
     return (
       <View>
+        <AllSwiper list={this.props.list} />
         <View className='column-wrap' style={{height:utils.height}}>
           {
             utils.columnList.map((column,index) => {
